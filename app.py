0f9fbc1c03e3b08be10b0a90f6c5a254e1719c15
neotheicebird <neotheicebird@gmail.com>
2018-04-07 00:28:30 +0530

    Functional 'buggy' version

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
-from flask import Flask, render_template
-from flask_ask import Ask, statement, question
-import requests
+# https://stackoverflow.com/a/44532317/2523414
+# no sqlite in lambda, needed by a small subset of nltk
+import imp
 # from readability import Document
 import os
-from pygoogling.googling import GoogleSearch
-import tldextract
-# from newspaper import Article
-# import lassie
+import sys
 
+import tldextract
+from flask import Flask, render_template
+from flask_ask import Ask, question
+from pygoogling.googling import GoogleSearch
+from summarizer.summarize import summarize
+
+# from newspaper import Article
 # patching stable version of newspaper for compatibility with AWS LAmbda. Can be removed in future
 # only /tmp is writable on lambda env
 # os.environ['NEWSPAPER_BASE_DIRECTORY'] = '/tmp'
 # os.environ['NLTK_DATA'] = '/tmp'
 # Setting the env vars from zappa_settings
-
-# https://stackoverflow.com/a/44532317/2523414
-# no sqlite in lambda, needed by a small subset of nltk
-import imp
-import sys
 sys.modules["sqlite"] = imp.new_module("sqlite")
 sys.modules["sqlite3.dbapi2"] = imp.new_module("sqlite.dbapi2")
 
+import newspaper
 from newspaper import Article
 # import nltk
 # nltk.download('punkt')
@@ -38,10 +38,10 @@ def launch():
     If user says e.g "Alexa, open I m feeling lucky"
     :return:
     """
-    card_title = render_template('launch_card_title').format(APP_NAME=APP_NAME)
-    reply = render_template('launch').format(APP_NAME=APP_NAME)
-    text = render_template('launch_card_text').format(APP_NAME=APP_NAME)
-    prompt = render_template('launch_card_prompt').format(APP_NAME=APP_NAME)
+    card_title = render_template('launch_card_title')
+    reply = render_template('launch')
+    text = render_template('launch_card_text')
+    prompt = render_template('launch_card_prompt')
     return question(reply).reprompt(prompt).simple_card(card_title, text)
 
 @ask.intent('feeling_lucky')
@@ -53,11 +53,21 @@ def feeling_lucky(search_query):
     try:
         search_request = GoogleSearch(search_query)
         search_request.start_search(max_page=1)
-        lucky_url = search_request.search_result.pop(0)
-        # webpage = requests.get(lucky_url)
-        article = Article(lucky_url)
-        article.download()
-        article.parse()
+        for x in range(5):    # Try upto 5 links from top. If all raises error, quit
+            try:
+                lucky_url = search_request.search_result.pop(0)
+                # webpage = requests.get(lucky_url)
+                article = Article(lucky_url)
+                article.download()
+                article.parse()
+                break
+            except newspaper.article.ArticleException:
+                print("Error encountered, Article parsing. URL: ", lucky_url)
+                continue
+        else:
+            # will run if break doesnt execute
+            return question("Sorry, I am unable to get proper results for this query. Please try something else")
+
         # resp = lassie.fetch(lucky_url)
 
         ext = tldextract.extract(lucky_url)
@@ -68,20 +78,21 @@ def feeling_lucky(search_query):
         print("$"*50)
         # content = resp["description"]
         # title = resp["title"]
-        content = article.summary if article.summary else article.text
         title = article.title
+        content = article.summary if article.summary else summarize(article.text, 2000)    # max words for a skill result is 8000
         print("*"*50)
         print(lucky_url, domain, content)
+        print(title)
         print("*" * 50)
     except IndexError:
         return question("Can you please repeat the query?")
 
-    card_title = render_template('result_card_title').format(APP_NAME=APP_NAME, search_query=search_query)
-    reply = render_template('result').format(APP_NAME=APP_NAME, content=content, domain=domain, title=title)
-    text = render_template('result_card_text').format(APP_NAME=APP_NAME, domain=domain, shortened_content=content[:125]+"...")
-    # prompt = render_template('launch_card_prompt').format(APP_NAME=APP_NAME)
+    card_title = render_template('result_card_title').format(search_query=search_query)
+    reply = render_template('result').format(content=content, domain=domain, title=title)
+    text = render_template('result_card_text').format(domain=domain, shortened_content=content[:125]+"...")
+    prompt = render_template('launch_card_prompt')
     # return question(reply).reprompt(prompt).simple_card(card_title, text)
-    return statement(reply).simple_card(card_title, text)
+    return question(reply).reprompt(prompt).simple_card(card_title, text)
 
 @ask.intent('AMAZON.HelpIntent')
 def help():
@@ -89,4 +100,5 @@ def help():
     # reprompt = render_template('help_prompt')
     text = render_template('help_card_text')
     title = render_template('help_card_title')
-    return statement(reply).simple_card(title, text)
+    prompt = render_template('launch_card_prompt')
+    return question(reply).reprompt(prompt).simple_card(title, text)
